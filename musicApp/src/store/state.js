7955cfe3555e80ec9732c3031900cb5e05f8d450
EvalGitHub <957010831@qq.com>
2017-12-24 09:34:10 +0800

    关于vux的更进一步的理解
    1）state中定义一些数据参数，格式
    2）在getters中对数据进行管理（具有对数据进行简单处理的能力）
    3）mutations主要提供一些方法，修改state，或者getter的数据

--- musicApp/src/store/state.js
+++ musicApp/src/store/state.js
@@ -1,8 +1,17 @@
 /**
  * Created by Administrator on 2017/12/4 0004.
  */
+import playMde from "common/js/config"
+
 const state={
-  singer:{}
-}
+  singer:{},
+
+  playing:false,//是否播放
+  fullScreen:false,//是否全屏
+  playList:[],//播放列表
+  sequenceList:[],//顺序播放表
+  mode:playMde.sequence,
+  currentIndex:-1//当前播放
+};
 
 export  default  state
