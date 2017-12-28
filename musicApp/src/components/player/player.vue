0265c5c205a8c5a5ef326faf1919f1f5a7edcd5f
EvalGitHub <957010831@qq.com>
2017-12-28 22:18:15 +0800

    使用js动画，第三方插件：create-keyframe-animation
    1）安装
    2）import animations from "create-keyframe-animation"
    3）
     let animation = {
              0: {
                transform: `translate3d(${x}px,${y}px,0) scale(${scale})`
              },
              60: {
                transform: `translate3d(0，0，0 scale(1.1)`
              },
              100: {
                transform: `translate3d(0,0,0 scale(1)`
              },
            };
    
            animations.registerAnimation({
              name: 'move',
              animation: animation,
              presets: {
                duration: 400,
                easing: 'linear'
              }
    
            });
            animations.runAnimation(this.$refs.cdWrapper, 'move', done)
    先编写animation样式，注册，运行（回掉函数）
    注意这里的回调函数执行的是done函数，他是vue中transition的动画钩子函数的默认回调函数，
    【当只用 JavaScript 过渡的时候， 在 enter 和 leave 中，回调函数 done 是必须的 。否则，它们会被同步调用，过渡会立即完成。】

--- musicApp/src/components/player/player.vue
+++ musicApp/src/components/player/player.vue
@@ -1,49 +1,56 @@
 <template>
   <div class="player" v-show="playList.length>0">
-    <!---正常的播放器-->
-    <div class="normal-player"  v-show="fullScreen">
-      <div class="background">
-        <img  alt="bg" width="100%" height="100%" :src="currentSong.image">
-      </div>
-      <div class="top">
-        <div class="back" @click="back">
-          <i class="icon-back"></i>
+    <transition name="normal"
+                @enter="enter"
+                @after-enter="afterEnter"
+                @leave="leave"
+                @after-leave="leaveEnter"
+    >
+      <!---正常的播放器-->
+      <div class="normal-player" v-show="fullScreen">
+        <div class="background">
+          <img alt="bg" width="100%" height="100%" :src="currentSong.image">
         </div>
-        <h1 class="title" v-html="currentSong.name"></h1>
-        <h2 class="subtitle" v-html="currentSong.singer"></h2>
-      </div>
-      <div class="middle">
-        <div class="middle-l">
-          <div class="cd-wrapper">
-            <div class="cd">
-              <img src="" alt="" class="image" :src="currentSong.image">
+        <div class="top">
+          <div class="back" @click="back">
+            <i class="icon-back"></i>
+          </div>
+          <h1 class="title" v-html="currentSong.name"></h1>
+          <h2 class="subtitle" v-html="currentSong.singer"></h2>
+        </div>
+        <div class="middle">
+          <div class="middle-l">
+            <div class="cd-wrapper" ref="cdWrapper">
+              <div class="cd">
+                <img src="" alt="" class="image" :src="currentSong.image">
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div class="bottom">
-        <div class="operators">
-          <div class="icon i-left">
-            <i class="icon-sequence"></i>
-          </div>
-          <div class="icon i-left">
-            <i class="icon-prev"></i>
-          </div>
-          <div class="icon i-center">
-            <i class="icon-play"></i>
-          </div>
-          <div class="icon i-right">
-            <i class="icon-next"></i>
-          </div>
-          <div class="icon i-right">
-            <i class="icon icon-not-favorite"></i>
+        <div class="bottom">
+          <div class="operators">
+            <div class="icon i-left">
+              <i class="icon-sequence"></i>
+            </div>
+            <div class="icon i-left">
+              <i class="icon-prev"></i>
+            </div>
+            <div class="icon i-center">
+              <i class="icon-play"></i>
+            </div>
+            <div class="icon i-right">
+              <i class="icon-next"></i>
+            </div>
+            <div class="icon i-right">
+              <i class="icon icon-not-favorite"></i>
+            </div>
           </div>
         </div>
+
       </div>
+    </transition>
 
-    </div>
-
-
+    <transition name="mini"></transition>
 
     <!--缩放后的播放器-->
     <div class="mini-player" v-show="!fullScreen" @click="open">
@@ -52,7 +59,7 @@
       </div>
       <div class="text">
         <h2 class="name" v-html="currentSong.name"></h2>
-        <p class="desc"  v-html="currentSong.singer"></p>
+        <p class="desc" v-html="currentSong.singer"></p>
       </div>
       <div class="control"></div>
       <div class="control">
@@ -65,6 +72,11 @@
 <script type="text/ecmascript-6">
   import {mapGetters, mapMutations} from 'vuex'
 
+  import animations from "create-keyframe-animation"
+
+  import {prefixStyle} from 'common/js/dom'
+
+  const transform= prefixStyle('transform');
 
   export default {
     name: ' ',
@@ -72,23 +84,82 @@
       return {}
     },
 
-    computed:{
+    computed: {
       ...mapGetters([
         'fullScreen',
         'playList',
         'currentSong'
       ])
     },
-    methods:{
-        back(){
-            this.setFullScreen(false)
-        },
+    methods: {
+      back(){
+        this.setFullScreen(false)
+      },
       open(){
-            this.setFullScreen(true)
+        this.setFullScreen(true)
       },
       ...mapMutations({
-        setFullScreen:'SET_FULL_SCREEN'
-      })
+        setFullScreen: 'SET_FULL_SCREEN'
+      }),
+
+      /*创建动画*/
+      enter(el, done){
+        const {x, y, scale} = this._getPosAndScale();
+        let animation = {
+          0: {
+            transform: `translate3d(${x}px,${y}px,0) scale(${scale})`
+          },
+          60: {
+            transform: `translate3d(0，0，0 scale(1.1)`
+          },
+          100: {
+            transform: `translate3d(0,0,0 scale(1)`
+          },
+        };
+
+        animations.registerAnimation({
+          name: 'move',
+          animation: animation,
+          presets: {
+            duration: 400,
+            easing: 'linear'
+          }
+
+        });
+        animations.runAnimation(this.$refs.cdWrapper, 'move', done)
+      },
+      afterEnter(){
+        animations.unregisterAnimation('move');
+        this.$refs.cdWrapper.style.animation = '';
+      },
+      leave(el, done){
+        this.$refs.cdWrapper.style.transition = 'all 0.4s'
+        const {x, y, scale} = this._getPosAndScale();
+        this.$refs.cdWrapper.style[transform]=`translate3d(${x}px,${y}px,0) scale(${scale})`
+        this.$refs.cdWrapper.addEventListener('transitionend',done)
+      },
+      leaveEnter(){
+          this.$refs.cdWrapper.style.transition='';
+        this.$refs.cdWrapper.style[transform]=''
+      },
+
+      /*获取图片的位置信息*/
+      _getPosAndScale(){
+        const targetWidth = 40
+        const paddingLeft = 40
+        const paddingBottom = 30
+        const paddingTop = 80
+        const width = window.innerHeight * 0.8
+        const scale = targetWidth / width
+        const x = -(window.innerWidth / 2 - paddingLeft)
+        const y = (window.innerHeight - paddingTop - width / 2 - paddingTop)
+        return {
+          x,
+          y,
+          scale,
+        }
+
+      }
     }
 
   }
