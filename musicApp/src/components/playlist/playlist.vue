009728d957e3de3db67f78a67fc179471dba21e7
EvalGitHub <957010831@qq.com>
2018-02-03 18:34:34 +0800

    computed与methods的区别
    1）computed与methods的区别
    computed是有缓存的，第二点methods中的方法是需要我们手动出发的，但是computed确实监听自动和计算的

---
+++ musicApp/src/components/playlist/playlist.vue
@@ -0,0 +1,56 @@
+<template>
+  <transition name="list-fade">
+    <div class="playlist">
+      <div class="list-wrapper">
+        <div class="list-header">
+          <h1 class="title">
+            <i class="icon"></i>
+            <span class="text"></span>
+            <span class="clear"><i class="icon-clear"></i></span>
+          </h1>
+        </div>
+
+        <div class="list-content">
+          <ul>
+            <li class="item">
+              <i class="current"></i>
+              <span class="text"></span>
+              <span class="like">
+                <i class="icon-not-favorite"></i>
+              </span>
+              <span class="delete">
+                <i class="icon-delete"></i>
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div class="list-operator">
+          <div class="add">
+            <i class="icon-add"></i>
+            <span class="text">添加歌曲队列</span>
+          </div>
+        </div>
+
+        <div class="list-close">
+          <span>关闭</span>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: ' ',
+    data () {
+      return {}
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+
+</style>
