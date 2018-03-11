08ddbbd7ae396c0841f4fd1f5cd40436deb66391
EvalGitHub <957010831@qq.com>
2018-03-11 12:17:41 +0800

    使用路由懒加载（按需加载路由对应的组件，性能优化）
    // import Recommend from '../components/recommend/recommend'
    // import Singer from '../components/singer/singer'
    // import Rank from '../components/rank/rank'
    // import Search from '../components/search/search'
    // import SingerDetail from '../components/singer-detail/singer-detail'
    // import Disc from '../components/disc/disc'
    // import TopList from "../components/top-list/top-list.vue"
    
    //路由懒加载（按需加载，这样可以减小app.js的文件大小，这是一个很好的优化点）
    const Recommend =()=> import('../components/recommend/recommend')
    const Singer =()=> import('../components/singer/singer')
    const Rank =()=> import('../components/rank/rank')
    const Search =()=> import('../components/search/search')
    const SingerDetail =()=> import( '../components/singer-detail/singer-detail')
    const Disc =()=> import('../components/disc/disc')
    const TopList =()=> import("../components/top-list/top-list.vue")

--- musicApp/src/router/index.js
+++ musicApp/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Recommend from '../components/recommend/recommend'
-import Singer from '../components/singer/singer'
-import Rank from '../components/rank/rank'
-import Search from '../components/search/search'
+// import Recommend from '../components/recommend/recommend'
+// import Singer from '../components/singer/singer'
+// import Rank from '../components/rank/rank'
+// import Search from '../components/search/search'
+// import SingerDetail from '../components/singer-detail/singer-detail'
+// import Disc from '../components/disc/disc'
+// import TopList from "../components/top-list/top-list.vue"
 
-import SingerDetail from '../components/singer-detail/singer-detail'
-
-import Disc from '../components/disc/disc'
-import TopList from "../components/top-list/top-list.vue"
+//路由懒加载（按需加载，这样可以减小app.js的文件大小，这是一个很好的优化点）
+const Recommend =()=> import('../components/recommend/recommend')
+const Singer =()=> import('../components/singer/singer')
+const Rank =()=> import('../components/rank/rank')
+const Search =()=> import('../components/search/search')
+const SingerDetail =()=> import( '../components/singer-detail/singer-detail')
+const Disc =()=> import('../components/disc/disc')
+const TopList =()=> import("../components/top-list/top-list.vue")
 
 Vue.use(Router);
 
